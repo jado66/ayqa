47ae4fc15caffa68d876a36dee191e802b213a8b
jado66 <77120464+jado66@users.noreply.github.com>
2023-03-21 23:37:37 -0600

    Fixed search btn styles

--- src/pages/[project]/images.js
+++ src/pages/[project]/images.js
@@ -82,7 +82,7 @@ const UploadImages = () => {
             <div className="input-group mb-3">
               <input type="text" className="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="basic-addon2"/>
               <div className="input-group-append">
-                <button className="btn btn-outline-secondary" type="button">Search</button>
+                <button className="btn btn-outline-secondary  rounded-0 rounded-end" type="button">Search</button>
               </div>
             </div>
           </form>
