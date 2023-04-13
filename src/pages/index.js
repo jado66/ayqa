905a19f232d0a64c5e5039e55e7d1e7b200082a3
cardonandrew <cardonandrew0@gmail.com>
2023-04-13 15:26:59 -0600

    fix: stylechanges

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,11 +27,11 @@ export default function Home({ providers }) {
         >
             
             <div className='page-container'>
-              <div className='segment-container1'>
-              <div className='blur'>
+            <div className='blur'>
+              <div className='segment-container2'>
               <div className='hor-width-container'>
                 <div className='info1'>
-                  <h2>Automate Quality Assurance in Manufacturing with AYQA</h2>
+                  <h3>Automate Quality Assurance in Manufacturing with AYQA</h3>
                   <br/>
                   <p>AYQA streamlines quality assurance in manufacturing through its innovative use of Machine Learning technology. Our node modules capture images of products to be evaluated by a neural network model hosted on a server. Actionable results can be easily triggered. Try a demo today.</p>
                   <br/>
@@ -43,11 +43,13 @@ export default function Home({ providers }) {
               </div>
               </div>
               </div>
-
-              <div className='segment-container2'>
+              <div className='blur'>
+              <div className='segment-container3'>
+              <div className='large-box'>
+              <div className='blur'>
               <div className='width-container'>
                 <div className='info2'>
-                  <h2>How AYQA Automates Quality Assurance</h2>
+                  <h1>How AYQA Automates Quality Assurance</h1>
                   <br/>
                   <p>AYQA automates the otherwise time-consuming task of manual quality assurance by enabling product images to be evaluated by a trained neural network model. Modules attached to the assembly line capture images throughout the production process, streamlining the detection of errors.</p>
                 </div>
@@ -63,19 +65,21 @@ export default function Home({ providers }) {
                     <p>Machine Learning models can identify defects with a higher degree of accuracy than human inspectors, reducing errors.</p>
                   </div>
                   <div className='cards'>
-                    <img className='small-container-image' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM3XowXzvH8DdtPJZ5Fd0_hEns4ybp8LFIKg&usqp=CAU'></img>
+                    <img className='small-container-image' src='https://images.squarespace-cdn.com/content/v1/5feb53185d3dab691b47361b/1609930650139-9NRI63XUJ29Y7E9LEA9G/12eca-machine-learning.gif'></img>
                     <h3>Increased Productivity</h3>
                     <p>Continuous monitoring ensures errors are detected early, allowing for quicker remediation, and increased productivity down the line.</p>
                   </div>
                  </div>
                  </div>
+                 </div>
+                 </div>
+                 </div>
                 </div>
-                <div className='segment-container3'>
-                <div className='large-box'>
                 <div className='blur'>
+                <div className='segment-container2'>
                 <div className='width-container'>
                 <div className='info3'>
-                  <h2>Node Modules in AYQA</h2>
+                  <h1>Node Modules in AYQA</h1>
                   <br/>
                   <p>The Node modules in AYQA are designed for easy integration with a company's existing manufacturing systems. The modules work in conjunction with a server and a trained Machine Learning model, ensuring quality assurance automation is streamlined for increased productivity.</p>
                 </div>
@@ -92,16 +96,14 @@ export default function Home({ providers }) {
                     <h4>Configurable to Specific Business Needs</h4>
                     <p>AYQA's Node Modules can be configured to match specific business needs, ensuring that they operate in a manner that is compatible with existing production systems ideal for business-specific manufacturing requirements.</p>
                   </div>
-                 
-                 </div>
-                 </div>
                  </div>
                 </div>
                 </div>
+              <hr></hr>
                 <div className='segment-container2'>
               <div className='width-container'>
                 <div className='info3'>
-                  <h2>Capturing Product Images with Node Modules</h2>
+                  <h1>Capturing Product Images with Node Modules</h1>
                   <br/>
                   <p>AYQA's device agnostic node modules allow images to be captured from any assembly line, capturing product images at any stage of production. The images are then used for analysis by the server-side Machine Learning model.</p>
                 </div>
@@ -112,8 +114,57 @@ export default function Home({ providers }) {
                 <img className='medium-container-image' src='https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1706&q=80'></img>
                  </div>
                  </div>
+                 </div>
+                  <hr></hr>
+                <div className='segment-container2'>
+                <div className='width-container'>
+                <div className='info3'>
+                  <h1>Training Your Machine Learning Model</h1>
+                  <br/>
+                  <p>AYQA’s Machine Learning module enables the creation of customized neural network models. In doing so, inspection is made more efficient due to a better understanding of the possible defects that might arise during production.</p>
                 </div>
+                <div className='cards-box'>
+                  <div className='cards3'>
+                    <h4>Quick & Easy Model Creation</h4>
+                    <p>Custom model creation is simplified through AYQA's library of Machine Learning models.</p>
+                  </div>
+                  <div className='cards3'>
+                    <h4>Training Data Preparation</h4>
+                    <p>A machine learning model is only as good as the data it's trained with. AYQA allows for the collection of high-quality, detailed datasets to assist with model training.</p>
+                  </div>
+                  <div className='cards3'>
+                    <h4>Iterative Model Training</h4>
+                    <p>The process of iterative model training continues throughout its usage to ensure maximum accuracy, with feedback from AYQA's intuitive reporting system guiding optimization.</p>
+                  </div>
+                 </div>
+                 </div>
+                 </div>
                 <hr></hr>
+                 <div className='segment-container2'>
+                <div className='width-container'>
+                <div className='info3'>
+                  <h1>Server-Side Evaluation of Product Images</h1>
+                  <br/>
+                  <p>Server-side evaluation of product images ensures quick and accurate decision-making for product quality. The evaluation is initiated through the modules' integration with the Machine Learning model on the server.</p>
+                </div>
+                <div className='cards-box'>
+                  <div className='cards2'>
+                    <h4>Realtime Evaluation</h4>
+                    <p>Images captured by the module are transmitted to the server for immediate assessment by the Machine Learning model, triggering the necessary measures in real-time.</p>
+                  </div>
+                  <div className='cards2'>
+                    <h4>Intuitive Error Alert System</h4>
+                    <p>The Machine Learning model of AYQA sends out alerts during its evaluation of product images, providing as much detail as possible about the error detected.</p>
+                  </div>
+                  <div className='cards2'>
+                    <h4>Realtime Analysis Reporting</h4>
+                    <p>Reports on the server-side evaluation of product images show a detailed analysis of every step of the production process, including a report of errors detected and their frequency.</p>
+                  </div>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+
             </div>
         </ProtectedContent>
       </main>
